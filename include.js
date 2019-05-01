d9d2795284b836cd9c31338c906fd017da26b243
janaka531 <janakapradeepedirisinghe@gmail.com>
2019-05-01 23:17:26 +0530

    11 17

--- include.js
+++ include.js
@@ -1,3 +1,5 @@
+<?php  session_start(); ?>
+
 public function(){
 	var x = document.form[0].element[0].value;
 	var y = document.form[0].element[1].value;
@@ -6,4 +8,8 @@ public function(){
 	 var salary = <?php echo $salary; ?>;
 	 var discount = <?php echo $discount ;?>;
 
+
+
+
 }
+
